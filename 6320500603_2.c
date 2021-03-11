ab82a97614382a1a4db222953e98fac7a5b178ff
icephat <phattaraporn.sa@ku.th>
2021-03-11 16:41:03 +0700

    เริ่มเขียนใหม่

--- 6320500603_2.c
+++ 6320500603_2.c
@@ -1,31 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j,k;
     scanf("%d",&n);
 
-    if(n%2!=0)
+    if(n%2==0)
     {
         for(i=0;i<n;i++)
         {
-            for(j=0;j<n;j++)
+            for(j=0;j<n/2;j++)
             {
-                printf("_");
+                printf("*");
             }
             printf("\n");
         }
     }
-    else
-    {
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<n-1;j++)
-            {
-                printf("_");
-            }
-            printf("\n");
-        }
-    }
+
+
 
 
 
