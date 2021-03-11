166dbd43bceb157788f93776d84dd23309eb7185
icephat <phattaraporn.sa@ku.th>
2021-03-11 17:45:45 +0700

    แก้ไขการทำให้เป็น0

--- 6320500603_1.c
+++ 6320500603_1.c
@@ -6,8 +6,34 @@ int main()
     int n,i,j,k;
     scanf("%d",&n);
 
-    int c[20]={};
-
+    char x[n];
+    int a[20];
+    for(i=0;i<20;i++)
+    {
+        a[i]=0;
+    }
+    char *p;
+    scanf("%[^\n]s",a);
+    p=strtok(a," ");
+    i=0;
+    while(p!=NULL)
+    {
+        if(!(strcmp(p,"+")))
+        {
+            a[i-2]+=a[i-1];
+            i--;
+        }
+        else if(!(strcmp(p,"-")))
+        {
+            a[i-2]-=a[i-1];
+            i--;
+        }
+        else if(!(strcmp(p,"*")))
+        {
+            a[i-2]*=a[i-1];
+            i--;
+        }
+    }
 
 
 
