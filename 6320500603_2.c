7418f09be9740761f0440b32e7658814bc30f428
icephat <phattaraporn.sa@ku.th>
2021-03-11 16:50:04 +0700

    คิดโค้ดใหม่

--- 6320500603_2.c
+++ 6320500603_2.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,k;
+    int n,a,x,i,j,k;
     scanf("%d",&n);
 
     if(n%2==0)
     {
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<n/2;j++)
-            {
-                printf("*");
-            }
-            printf("\n");
-        }
+        n--;
+        a++;
+        x=((n+a)/2)-1;
+    }
+    else
+    {
+        x=((n+a)/2);
     }
 
 
