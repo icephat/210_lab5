438f5b5021e545cf1dac911b2881fb979b1cef59
icephat <phattaraporn.sa@ku.th>
2021-03-11 16:53:04 +0700

    คิดโค้ดโดยแบ่งเป็น3เหลี่ยมท่อนบน

--- 6320500603_2.c
+++ 6320500603_2.c
@@ -15,6 +15,21 @@ int main()
         x=((n+a)/2);
     }
 
+    for(i=0;i<(n+a)/2;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(j==(n/2)-i||j==(n/2)+i)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf("_");
+            }
+        }
+        printf("\n");
+    }
 
 
 
